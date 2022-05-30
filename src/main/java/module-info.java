8e923976f1bb0ca7c74a987331db026f090ec718
Yakshi Gandhi <200482015@student.georgianc.on.ca>
2022-05-30 15:54:16 -0400

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.assignment1gc200482015 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example.assignment1gc200482015 to javafx.fxml;
+    exports com.example.assignment1gc200482015;
+}
